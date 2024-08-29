ff693ab53ebd6343ade41d360095585cb82e23df
TommasoParenti <t.parenti1@studenti.unipi.it>
2024-08-29 18:02:45 +0200

    Piccola correzione su SW

--- dist/StudyMatch/browser/ngsw-worker.js
+++ dist/StudyMatch/browser/ngsw-worker.js
@@ -1,3 +1,40 @@
+
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = "offline";
+const OFFLINE_URL = "assets/offline.html";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      await cache.add(new Request(OFFLINE_URL, { cache: "reload" }));
+    })()
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(OFFLINE_URL);
+            return cachedResponse;
+        }
+      })()
+    );
+  }
+});
+
 (() => {
   var __defProp = Object.defineProperty;
   var __defProps = Object.defineProperties;
